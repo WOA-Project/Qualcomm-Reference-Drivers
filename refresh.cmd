bf4d8049578cadcfe0ad3caeb44dee4349a24666
Gustave Monce <gustave.monce@outlook.com>
2025-11-11 12:13:34 +0100

    Fix: Targeting minimum version to avoid bad "preview update" update ids coming in now

--- refresh.cmd
+++ refresh.cmd
@@ -1,4 +1,4 @@
 @echo off
 cd tools
-"UUPDownload.%PROCESSOR_ARCHITECTURE%.exe" request-bsp-download2 -s Professional -v 10.0.26200.2483 -t arm64 -r Retail -b Retail -a CB -c ge_release -o "%CD%\.."
+"UUPDownload.%PROCESSOR_ARCHITECTURE%.exe" request-bsp-download2 -s Professional -v 10.0.26300.2483 -t arm64 -r Retail -b Retail -a CB -c ge_release -o "%CD%\.."
 cd ..
